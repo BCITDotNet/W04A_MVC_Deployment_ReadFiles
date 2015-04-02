6905821667d655b9fb6133db58427c8ef77d5da5
A00897783 <bd0421@gmail.com>
2015-04-02 19:33:47 +0000

    initial commit

---
+++ Controllers/FilesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using System.Web;
+
+namespace MvcPrimer.Controllers
+{
+    public class FilesController : Controller
+    {
+        // GET: Files
+        public ActionResult Index()
+        {
+            string[] files = Directory.GetFiles(@"C:\inetpub\wwwroot\w04a\MvcPrimer\somedata")
+                 .Select(path => Path.GetFileName(path)).ToArray();
+
+            ViewData["files"] = files;
+
+            //ViewBag.MyMessage = "This is my process index action method";
+            return View();
+        }
+
+        public ActionResult Content(string filename)
+        {
+
+            string filepath = Server.MapPath("../somedata/") + filename;
+            System.IO.StreamReader rdr = new System.IO.StreamReader(filepath);
+            ViewData["data"] = rdr.ReadToEnd();
+            rdr.Close();
+
+            return View();
+
+        }
+    }
+}
